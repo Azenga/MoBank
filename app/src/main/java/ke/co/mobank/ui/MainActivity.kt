e585bf4d065677f441e2147efde12be7eef106af
Azenga Kevin <toomuch573@gmail.com>
2021-01-04 02:06:52 +0300

    Wip, navigation UI hookup and options menu update

--- app/src/main/java/ke/co/mobank/ui/MainActivity.kt
+++ app/src/main/java/ke/co/mobank/ui/MainActivity.kt
@@ -1,22 +1,37 @@
 package ke.co.mobank.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.firebase.auth.FirebaseAuth
 import ke.co.mobank.R
+import ke.co.mobank.ui.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,7 +39,23 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.options_menu, menu)
 
         return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.sign_out_item) {
+            firebaseAuth.signOut()
+            sendToLogin()
+            return true
+        }
+
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    private fun sendToLogin() {
+        Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }.also { startActivity(it) }
     }
 
 }
