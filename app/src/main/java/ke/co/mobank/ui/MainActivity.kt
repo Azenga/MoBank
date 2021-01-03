58ef4696434dce57b53b4d6d2a9127aab26e25cc
Azenga Kevin <toomuch573@gmail.com>
2021-01-04 01:18:42 +0300

    Wip, hooking up the navigation resource file and the options menu file to the main activity

--- app/src/main/java/ke/co/mobank/ui/MainActivity.kt
+++ app/src/main/java/ke/co/mobank/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package ke.co.mobank.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.firebase.auth.FirebaseAuth
 import ke.co.mobank.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        return true
+
+    }
+
 }
