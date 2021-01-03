7222ec573375feec63df7e369794720a17e48356
Azenga Kevin <toomuch573@gmail.com>
2021-01-03 13:18:08 +0300

    Wip, added authentication views

---
+++ app/src/main/java/ke/co/mobank/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package ke.co.mobank.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ke.co.mobank.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
