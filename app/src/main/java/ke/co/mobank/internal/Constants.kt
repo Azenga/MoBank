6d136568e50127d4d67056b6ad3f40dfc73045ec
Azenga Kevin <toomuch573@gmail.com>
2021-01-04 17:07:43 +0300

    Wip, designing add transactions layouts

---
+++ app/src/main/java/ke/co/mobank/internal/Constants.kt
@@ -0,0 +1,8 @@
+package ke.co.mobank.internal
+
+object Constants {
+
+    val TRANSACTION_TYPES = arrayOf("Withdraw", "Deposit")
+
+    val PLATFORMS = arrayOf("M-Pesa", "Airtel", "Telecom", "Equity", "KCB", "Cooperative")
+}
--- app/src/main/java/ke/co/mobank/ui/transactions/add/TransactionDetailsFragment.kt
+++ app/src/main/java/ke/co/mobank/ui/transactions/add/TransactionDetailsFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import ke.co.mobank.R
 import ke.co.mobank.databinding.FragmentTransactionDetailsBinding
+import ke.co.mobank.internal.Constants
 
 class TransactionDetailsFragment : Fragment() {
 
@@ -26,7 +28,23 @@ class TransactionDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val typeAdapter = ArrayAdapter<String>(
+            requireContext(),
+            R.layout.dropdown_menu_popup_item,
+            Constants.TRANSACTION_TYPES
+        )
+        binding.transactionTypeField.setAdapter(typeAdapter)
+
+        val platformAdapter = ArrayAdapter<String>(
+            requireContext(),
+            R.layout.dropdown_menu_popup_item,
+            Constants.PLATFORMS
+        )
+        binding.transactionPlatformField.setAdapter(platformAdapter)
+
         navController = Navigation.findNavController(view)
+
+
 
         binding.finishButton.setOnClickListener { navController.navigate(R.id.action_transaction_stored) }
     }
